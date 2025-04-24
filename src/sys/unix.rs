39afc813a6dc4f0234670b1fc786af890f214f8a
t6tb7n <t6tb7n@gmail.com>
2025-04-24 17:17:47 +0200

    Improve support for QNX Neutrino with iosock

--- src/sys/unix.rs
+++ src/sys/unix.rs
@@ -280,7 +280,7 @@ pub(crate) type Bool = c_int;
     target_os = "ios",
     target_os = "visionos",
     target_os = "macos",
-    target_os = "nto",
+    all(target_os = "nto", any(target_env = "nto71", target_env = "nto70")),
     target_os = "tvos",
     target_os = "watchos",
 ))]
@@ -290,7 +290,7 @@ use libc::TCP_KEEPALIVE as KEEPALIVE_TIME;
     target_os = "ios",
     target_os = "visionos",
     target_os = "macos",
-    target_os = "nto",
+    all(target_os = "nto", any(target_env = "nto71", target_env = "nto70")),
     target_os = "openbsd",
     target_os = "tvos",
     target_os = "watchos",
